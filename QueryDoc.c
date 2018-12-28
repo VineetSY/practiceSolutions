20de8111a65940a81d7ca93bc32369889a49dcd2
Vineet Yadav <93vins@gmail.com>
2018-12-28 12:09:53 +0530

    Update QueryDoc.c

--- QueryDoc.c
+++ QueryDoc.c
@@ -7,11 +7,11 @@
 #define MAX_PARAGRAPHS 5
 
 char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
-    return document[k-1][m-1][n-1];
+    return document[n-1][m-1][k-1];
 }
 
 char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) { 
-    return document[k-1][m-1];
+    return document[m-1][k-1];
 }
 
 char*** kth_paragraph(char**** document, int k) {
